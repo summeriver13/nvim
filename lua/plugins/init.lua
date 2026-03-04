f9d4112262726833c80c2712ac45817c30d4b95f
JIANG XIA <xia.jiang.code@outlook.com>
2026-03-05 00:26:00 +0800

    feat(plugins): 添加 nvim-web-devicons 插件
    新增 nvim-web-devicons 插件并配置默认图标集，以在文件树等界面中显示文件类型图标，增强视觉辨识度。

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
+  "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("nvim-web-devicons").setup({
+      default = true,
+    })
+  end,
   "folke/neodev.nvim",
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
